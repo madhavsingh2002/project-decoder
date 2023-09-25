50cae358ecbafd538d69164666295af8e5a77e13
madhav <madhav201singh@gmail.com>
2023-09-25 11:10:07 +0530

    register-endpoints

--- project-decode-backend-2/index.js
+++ project-decode-backend-2/index.js
@@ -2,6 +2,74 @@ const express =require('express')
 const connectToMongodb = require('./config/db.js');
 const app =express()
 connectToMongodb();
+const bcrypt = require('bcryptjs');
+
+// test-endpoints
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+// User-EndPoints
+
+// Define the registerController as a route handler
+app.post("/register", async (req, res) => {
+    try {
+      const { name, email, password, phone, address, answer } = req.body;
+      
+      // Validations (You can use a function for this if needed)
+      if (!name || !email || !password || !phone || !address || !answer) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+      
+      // Check if the user already exists
+      const existingUser = await userModel.findOne({ email });
+  
+      if (existingUser) {
+        return res.status(200).json({
+          success: false,
+          message: "User already registered. Please log in.",
+        });
+      }
+  
+      // Hash the password
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+  
+      // Register the user
+      const user = await new userModel({
+        name,
+        email,
+        phone,
+        address,
+        password: hashedPassword,
+        answer,
+      }).save();
+  
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        user,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: "Error in registration",
+        error: error.message,
+      });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8000,()=>{
     console.log('server is running on 8000')
--- project-decode-backend-2/models/categoryModel.js
+++ project-decode-backend-2/models/categoryModel.js
@@ -1,18 +1,23 @@
 // Create a Category model for an e-commerce platform with essential fields
 // including name, slug.
 
-import mongoose from "mongoose";
-
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    // required: true,
-    // unique: true,
-  },
-  slug: {
-    type: String,
-    lowercase: true,
-  },
-});
-
-export default mongoose.model("Category", categorySchema);
+// This is the problem statement...
+// we have build the product, order, user model so far..
+// now let's build the category modelllll.
+// It's quite simple .
+const mongoose = require('mongoose')
+const categorySchema =  new mongoose.Schema({
+    name:{
+      type:String,
+      required:true,
+      unique:true,
+    },
+    slug:{
+      type:String,
+      lowercase: true,
+    }
+})
+// now import this...,// this is the simple way to create the category model 
+// for the ecommerce.....
+const CategoryModel = mongoose.model('Category',categorySchema)
+module.exports =CategoryModel; 
