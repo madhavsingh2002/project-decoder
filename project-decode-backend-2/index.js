b6c6609e1b069b5457417c6939dc7e6ddf08e761
madhav <madhav201singh@gmail.com>
2023-09-27 18:21:35 +0530

    update

--- project-decode-backend-2/index.js
+++ project-decode-backend-2/index.js
@@ -14,7 +14,7 @@ app.get('/',(req,res)=>{
     res.send('hello world')
 })
 
-
+//
 // Define the JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -64,65 +64,60 @@ app.post('/register',async(req,res)=>{
 
 
 // Define the loginController as a route handler
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    
-    // Validation
-    if (!email || !password) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid email or password",
-      });
-    }
 
-    // Check if the user exists
-    const user = await UserModel.findOne({ email });
+// Let's the endpoint for login,
+// we have create register entpoint so far,
+// Let's create this...
 
-    if (!user) {
+app.post('/login',async(req,res)=>{
+  try{
+    const {email,password}=req.body;
+    // validation.
+    if(!email || !password){
       return res.status(404).json({
-        success: false,
-        message: "Email is not registered",
-      });
+        success:false,
+        message:'Invalid email or password'
+      })
     }
-
-    // Compare passwords
-    const match = await bcrypt.compare(password, user.password);
-
-    if (!match) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid password",
+      // check if the user exists.
+      const user = await UserModel.findOne({email});
+      if(!user){
+        return  res.status(404).json({
+          success:false,
+          message:'Email is not registered'
+        })
+      }
+      // Compare passwords.
+      const match = await bcrypt.compare(password,user.password)
+      if(!match){
+        return res.status(404).json({
+          success:false,
+          message:'invalid password'
+        })
+      }
+      // Generate a jwt token.
+      const token = jwt.sign({_id:user.id},JWT_SECRET,{
+        expiresIn:'7d'
       });
-    }
-
-    // Generate a JWT token
-    const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-      expiresIn: "7d",
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "Login successful",
-      user: {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        address: user.address,
-        role: user.role,
-      },
-      token,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: "Error in login",
-      error: error.message,
-    });
+      res.status(200).json({
+        success:true,
+        message:'Login successfully',
+        user:{
+          _id:user._id,
+          name:user.name,
+          email:user.email,
+          phone:user.phone,
+          address:user.address,
+          role:user.role,
+        },
+        token
+      })
+    // let's test this...,it's working fine, thank's for watching...
   }
-});
+  catch(err){
+    res.status(505).json({error:err.message})
+  }
+})
 
 
 app.listen(8000,()=>{
